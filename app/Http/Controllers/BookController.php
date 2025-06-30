bdd94a7bf08e2de04467b0d79a4514ea70408568
balzhankomekbaeva <230895@astanait.edu.kz>
2025-06-30 15:33:28 +0500

    created models Book and Author, added components like header and footer, and worked with one to many relationship

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,11 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
 use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
     public function show($id){
+//        $books = Book::all();
+//        dd($books);
         return view('books.index', ['id' => $id]);
     }
+
+//    public function show($id){
+//        $book = Book::find($id);
+//        dd($book->author);
+//        return view('books.index', ['id' => $id]);
+//    }
 }
